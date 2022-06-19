514d14ad10c11ec34c90c5d497c933401d98bb04
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-06-19 17:18:20 +0500

    feat: Add first component and setup text-editor

---
+++ my-react-app/src/components/16-First-Component/Greeting.jsx
@@ -0,0 +1,15 @@
+import { React } from "react";
+// import  ReactDOM  from "react-dom";
+
+export default function Greeting() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+/* ReactDOM.render(
+  <Greeting/>,
+  document.getElementById("root")
+) */
---
+++ my-react-app/src/index.jsx
@@ -0,0 +1,10 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import Greeting from "./components/16-First-Component/Greeting";
+
+ReactDOM.render(
+  <StrictMode>
+    <Greeting />
+  </StrictMode>,
+  document.getElementById("root")
+);
