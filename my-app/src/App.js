4e76c3cd3198a382c2dfef1140ed697977a2dd87
islomnumanovuz <islomnumanov.uz@gmail.com>
2022-07-05 21:07:35 +0500

    feat: Add useEffect function

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UseEffect from "./components/47-useEffect Basics/UseEffect";
+
+function App() {
+  return (
+    <div className="App">
+      <UseEffect />
+    </div>
+  );
+}
+
+export default App;
